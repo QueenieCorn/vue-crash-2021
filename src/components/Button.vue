906e397ff5abea3247d6be3a8930511d410f525f
QueenieCorn <queenielodonia@gmail.com>
2021-04-12 00:39:13 +0800

    files

---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+    <button @click="onClick()" :style="{ background:color }" class="btn"> {{text}} </button> 
+    <!-- v-on:click="onClick() or @click="onClick()" " -->
+
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props : {
+        text:String,
+        color: String
+    },
+    methods: {
+        onClick() {
+            console.log('click')
+        }
+    }
+}
+</script>
+
+<style>
+    
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template lang="">
+
+    <header>
+            <h1>{{ title }}</h1>
+            <Button text="Add Task" color="green"/>
+            <!-- <Button text="Update Task" color="blue"/>
+            <Button text="Delete Task" color="red"/> -->
+    </header>
+
+</template>
+<script>
+
+import Button from './Button'
+
+export default {
+    name: 'Header',
+    // props: ['title'],
+    props: {
+        title: String,
+        // title: {
+        //     type: String,
+        //     default: 'Hello World',
+        // }
+    },
+    components: {
+        Button,
+    }
+}
+</script>
+
+<style scoped>
+    header{
+        display: flex;
+        justify-content:space-between;
+        align-content: center;
+        margin-bottom: 20px;
+    }
+</style>
